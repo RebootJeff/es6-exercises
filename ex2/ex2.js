a732ffabb985923ccc0593fdb26e1a425583f807
Jeffrey Lee <RebootJeff@users.noreply.github.com>
2016-02-12 11:31:29 -0800

    ex2 first attempt

--- ex2/ex2.js
+++ ex2/ex2.js
@@ -1,10 +1,12 @@
-function foo() { }
+function foo(x, y, z, ...others) {
+	return [x, ...others];
+}
 
 function bar() {
 	var a1 = [ 2, 4 ];
 	var a2 = [ 6, 8, 10, 12 ];
 
-	return foo();
+	return foo(...a1, ...a2);
 }
 
 console.log(
