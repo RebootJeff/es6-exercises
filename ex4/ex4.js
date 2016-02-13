48149b9f64471c52b089f9b7f6625ad5cb5722c8
Jeffrey Lee <RebootJeff@users.noreply.github.com>
2016-02-12 16:16:56 -0800

    ex4 first attempt

--- ex4/ex4.js
+++ ex4/ex4.js
@@ -1,10 +1,22 @@
-function upper(strings,...values) {}
+function upper(strings,...values) {
+	let str = '';
+
+	strings.forEach((segment, index) => {
+		if(index > 0) {
+			let value = values[index - 1];
+			str += value.charAt(0).toUpperCase() + value.slice(1);
+		}
+		str += segment;
+	});
+
+	return str;
+}
 
 var name = "kyle",
 	twitter = "getify",
 	classname = "es6 workshop";
 
 console.log(
-`Hello ____ (@____),
-welcome to the ____!`
+upper`Hello ${name} (@${twitter}),
+welcome to the ${classname}!`
 );
